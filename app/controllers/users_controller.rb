87c4bcfff29df8ad3560553c652fba38cc286ce9
Shane Nicholson <shanescottnicholson@gmail.com>
2022-05-03 19:58:51 -0700

    Adds crud actions to users for index and create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+
+  def index
+    users = User.all 
+    render json: users.as_json
+  end
+
+  def create
+    user = User.new(username: params[:username], email: params[:email], password: params[password])
+    if user.save
+      render json: user.as_json
+    else
+      render json: {error: user.error.full_messages}
+    end
+  end
 end
